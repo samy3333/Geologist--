7b3a9e9b36ce2be59e604138b716558281b186c0
samy3333 <70003427+samy3333@users.noreply.github.com>
2021-02-04 20:01:16 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,6 +24,13 @@ function setup() {
 	 hammer= new Hammer(200, 200, 70, 50);
 	 rubber= new Rubber(400, 500, 90, 30);
 	 plane= new Plane(400, 700, 800, 50);
+	 stone = new Stone(700,320,100,100);
+
+	 //	sand1 = new Sand(300,500,10);
+   // sand2 = new Sand(310,500,10);
+ //   sand3 = new Sand(315,500,10);
+   // sand4 = new Sand(320,500,10);
+    //sand5 = new Sand(325,500,10);
 
 	Engine.run(engine);
   
@@ -37,6 +44,12 @@ function draw() {
 hammer.display(); 
 rubber.display();
 plane.display();
+stone.display()
+//sand1.display();
+//sand2.display();
+//sand3.display();
+//sand4.display();
+//sand5.display();
 }
 
 
