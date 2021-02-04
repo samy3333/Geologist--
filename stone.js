39613ad5220fcfceee5b12d3cb21dedd032400a2
samy3333 <70003427+samy3333@users.noreply.github.com>
2021-02-04 19:58:44 +1100

    Update stone.js

--- stone.js
+++ stone.js
@@ -1,28 +1,25 @@
-class Stone{
-    constructor(x,y,rad)
-      {
-          var options={
-              restitution:0.4,
-              friction:5,
-              density:1
-          }
-          this.x=x;
-          this.y=y;
-          this.rad=rad
-          this.body=Bodies.circle(this.x, this.y, (this.rad-20)/2, options)
-          World.add(world, this.body);
-  
+class Stone {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':5,
+          'density':1
       }
-      display()
-      {
-              var pos=this.body.position;		
-              push()
-              translate(pos.x, pos.y);
-              rectMode(CENTER);
-              fill("red");
-              ellipse(0,0,this.rad, this.rad);
-              pop()
-              
-      }
-  
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill("blue");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
   }
